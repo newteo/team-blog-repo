00b5f7a19e28f021d5d6e9c2ee3bc9f12771cc9a
wardenger <wardenger@gmail.com>
2016-09-24 18:28:40 +0800

    a

--- containers/Root.js
+++ containers/Root.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { Provider } from 'react-redux'
 import routes from '../routes'
 import { Router } from 'react-router'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 
 export default class Root extends Component {
   render() {

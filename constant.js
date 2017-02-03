1095ac16c4c7e1e1bc36b1e096b7c58d5ce0c3f4
wardenger <wardenger@gmail.com>
2017-02-03 11:07:09 +0800

    簡化 configureStore.js 代碼

--- constant.js
+++ constant.js
@@ -1,4 +1,3 @@
-
-
+// 去掉多餘空行，保留最後一行空行
 
 export const ARTICLES_API = 'https://api.github.com/repos/newteo/team-blog-repo/issues'
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,18 +1,12 @@
-/*if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
-}
-*/
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-
+import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
-const configureStore = (preloadedState) => createStore(  
-  	rootReducer,
-  	preloadedState,
-  	applyMiddleware(thunkMiddleware)
-  ) 
+const configureStore = (preloadedState) => createStore(
+  rootReducer,
+  preloadedState,
+  applyMiddleware(thunkMiddleware)
+)
 
 export default configureStore
